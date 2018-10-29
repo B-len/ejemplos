0892617e0b4f005b1da1837a98c44e871d3f6186
Belen Gamer <mary.belencatorce@gmail.com>
2018-10-29 15:35:53 +0100

    ejercicios

---
+++ html/src/main.js
@@ -0,0 +1,41 @@
+// console.log(1);
+// console.log('1');
+// console.log(true);
+// var miVariable;
+//console.log(miVariable);
+
+
+/*
+console.log({
+  name:"Belen",
+  bool:true,
+  defined:undefined,
+  miVariable:miVariable
+  });
+  */
+
+
+// var miObjeto ={
+//     'name':'Belen',
+//     bool:true,
+// };
+// console.log(miObjeto);
+// console.log(miObjeto.name);
+// console.log(miObjeto['bool']);
+
+// var arr=new Array(1,2,3,'hola');
+// var arr2=[1,2,3,'hola'];
+//
+// arr[5]='indice 5';
+// console.log('Array indice 5 ->',arr[5]);
+// console.log('Array indice 4 ->',arr[4]);
+//
+// var miVar=1+1;
+// var variable4=prompt('Escrbe un valor');
+// console.log(variable4);
+// console.log(parseInt(variable4)+1);
+// console.log(new Date(1000));
+//
+ console.log('1'+1);
+
+
